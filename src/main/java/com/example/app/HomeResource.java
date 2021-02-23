6f8265f1d4b68d291984a3c655f1aa650070ce8e
Hageromo <44583424+Hageromo@users.noreply.github.com>
2021-02-23 03:24:35 +0100

    Add files via upload

---
+++ src/main/java/com/example/app/HomeResource.java
@@ -0,0 +1,24 @@
+package com.example.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String home(){
+        return ("<h1>Hello Home</h1>");
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return ("<h1>Hello user</h1>");
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return ("<h1>Hello admin</h1>");
+    }
+
+}
